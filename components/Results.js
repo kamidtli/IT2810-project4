3620b3b7f0da2ebbd6274b29ea727d671b2b13e9
Kim <kmidtlid3@gmail.com>
2019-10-31 17:49:00 +0100

    #10 Added list divider

--- components/Results.js
+++ components/Results.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, FlatList, ActivityIndicator, } from 'react-native';
+import { View, FlatList, ActivityIndicator } from 'react-native';
+import { Divider } from 'react-native-elements';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import MovieDetail from '../components/MovieDetail';
@@ -28,24 +29,30 @@ export default function Results({query}) {
     { variables: { searchValue: query },
   });
 
+  renderDivider = () => {
+    return (
+      <Divider />
+    );
+  }
+
   if (loading) return <ActivityIndicator size="large" />;
   if (error) return <Text>{error.message}</Text>;
 
   return (
-      <FlatList
-      data={data.filterMovies}
-      renderItem={({item}) => (
-          <View>
-          <MovieDetail
-              movieID={item._id}
-              title={item.title}
-              rating={item.imdb.rating}
-              poster={item.poster}
-              />
-          </View>
-      )}
-      keyExtractor={(item) => item._id}
-      ItemSeparatorComponent={this.renderDivider}
-      />
+    <FlatList
+    data={data.filterMovies}
+    renderItem={({item}) => (
+        <View>
+        <MovieDetail
+            movieID={item._id}
+            title={item.title}
+            rating={item.imdb.rating}
+            poster={item.poster}
+            />
+        </View>
+    )}
+    keyExtractor={(item) => item._id}
+    ItemSeparatorComponent={this.renderDivider}
+    />
   )
 }
