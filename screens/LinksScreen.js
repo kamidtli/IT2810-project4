0a4dd6eb1d6f4cdf593ba0cdd0e8f6235aec91e8
eirsture <eirik.sture@gmail.com>
2019-11-02 19:30:38 +0100

    #5 Added list of moviedetails from stored watchlist to watchlist page

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,27 +1,59 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import { connect } from 'react-redux';
+import {
+  SafeAreaView,
+  View,
+  ScrollView,
+  FlatList,
+  Text,
+  StyleSheet,
+  StatusBar
+} from 'react-native';
+import { Divider, Icon } from 'react-native-elements';
+import MovieDetail from '../components/MovieDetail';
 
-export default function LinksScreen() {
-  return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
-  );
+function LinksScreen(props) {
+  console.log(props.watchlist);
+
+  if (!props.watchlist) {
+    return (
+      <View>
+        <Text h1>No videos in watchlist</Text>
+      </View>
+    );
+  } else {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={props.watchlist}
+          renderItem={({ item }) => (
+            <View>
+              <MovieDetail
+                movieID={item._id}
+                title={item.title}
+                rating={item.imdb.rating}
+                poster={item.poster}
+              />
+            </View>
+          )}
+          keyExtractor={item => item._id}
+          ItemSeparatorComponent={this.renderDivider}
+        />
+      </SafeAreaView>
+    );
+  }
 }
 
-LinksScreen.navigationOptions = {
-  title: 'Links',
-};
+const mapStateToProps = state => ({
+  watchlist: state.watchlist
+});
+
+export default connect(mapStateToProps)(LinksScreen);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
     backgroundColor: '#fff',
-  },
+    paddingTop: StatusBar.currentHeight
+  }
 });
