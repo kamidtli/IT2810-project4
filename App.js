0ae205a94074499ce8cc135f4176519460d766ce
Kim <kmidtlid3@gmail.com>
2019-11-05 17:35:48 +0100

    #12 Removed unused imports

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
+import {AppLoading} from 'expo';
+import {Asset} from 'expo-asset';
 import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Provider } from 'react-redux';
+import React, {useState} from 'react';
+import {Provider} from 'react-redux';
 import store from './redux/store';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { ApolloProvider } from '@apollo/react-hooks';
+import {Platform, StatusBar, StyleSheet, View} from 'react-native';
+import {Ionicons} from '@expo/vector-icons';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {ApolloProvider} from '@apollo/react-hooks';
 
 import AppNavigator from './navigation/AppNavigator';
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: 'http://it2810-16.idi.ntnu.no:4000/graphql'
+  uri: 'http://it2810-16.idi.ntnu.no:4000/graphql',
 });
 
 const client = new ApolloClient({
   cache,
-  link
+  link,
 });
 
 export default function App(props) {
@@ -50,17 +50,13 @@ export default function App(props) {
 
 async function loadResourcesAsync() {
   await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png')
-    ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
       ...Ionicons.font,
       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
       // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
-    })
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+    }),
   ]);
 }
 
@@ -77,6 +73,6 @@ function handleFinishLoading(setLoadingComplete) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
-  }
+    backgroundColor: '#fff',
+  },
 });
